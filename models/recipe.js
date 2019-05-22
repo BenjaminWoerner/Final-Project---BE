470beac97bdd0b4c4c0dafdd0bcd56a1ce35a1dc
BenjaminWoerner <ben.woerner@gmail.com>
2019-05-22 11:15:08 +0200

    initital commit

---
+++ models/recipe.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+ creator: {type: Schema.Types.ObjectId, ref: 'User'},
+ name: String,
+ description: String,
+ images: [],
+ tags: [{
+  diet: {type: String, enum: ['Keto', 'Low-Carb', 'High-Protein', 'Vegan', 'Vegetarian']},
+  component: String,
+  type: String,
+  cuisine: String,
+  mainIngredient: []
+  }],
+ comments: [{
+  creator: {type: Schema.Types.ObjectId, ref: 'User'},
+  text: String,
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+  }],
+yield: {
+    quantityPrimary: Number,
+    unitPrimary: String,
+    quantitySecondary: Number,
+    unitSecondary: String
+    },
+ingredients: [{
+  name: String,
+  quantity: Number,
+  unit: String,
+  prep: String,
+  comment: String,
+  category: {type: String, enum: ['Veg', 'Meat', 'Poultry', 'Fish', 'Pantry']},
+   }],
+method: [],
+timestamps: {
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+}
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
